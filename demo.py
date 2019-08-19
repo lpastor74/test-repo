f32cc2f836636f11352128f9e8d7557f84986561
Laslo <plaslo@gmail.com>
2019-08-19 17:04:28 -0500

    testing the VS

---
+++ demo.py
@@ -0,0 +1,14 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+# print(sys.executable)
+print(sys.version)
+print('Hello')
+test = 'Test'
+r = requests.get('https://github.com/timeline.json')
+print(r.status_code)
+print(requests.codes.teapot)
+print(r.text)
